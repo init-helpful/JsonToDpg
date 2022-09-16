92272209fae40a802d48b03784b4369fb405c43f
init-helpful <80361624+init-helpful@users.noreply.github.com>
2022-09-16 10:05:01 -0400

    uptick

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="jsontodpg",
-    version="1.7",
+    version="1.9",
     description="Build DearPyGui user interface with json/python dictionaies",
     author="init-helpful",
     author_email="init.helpful@gmail.com",

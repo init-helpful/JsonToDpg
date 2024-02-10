60953d594ffc7f659ad9c4399c58e232dc7b4dac
init-helpful <80361624+init-helpful@users.noreply.github.com>
2024-02-10 15:20:31 -0500

    Cleanup - Async Functions

--- src/jsontodpg.py
+++ src/jsontodpg.py
@@ -1,4 +1,5 @@
 from tokenizer import Tokenizer
+from asyncfunction import AsyncFunction
 from dpgkeywords import *
 import dearpygui.dearpygui as dpg
 # from threading import Thread
@@ -38,25 +39,6 @@ def children(obj):
 
 
 
-class AsyncFunction:
-    def __init__(
-        self,
-        interval,
-        function_reference,
-        end_condition,
-        pause_condition,
-        cycles=0,
-    ):
-        self.interval = interval
-        self.function_reference = function_reference
-        self.cycles = cycles
-        self.times_performed = 0
-        self.end_condition = end_condition if end_condition else lambda : False
-        self.pause_condition = pause_condition if pause_condition else lambda : False
-
-    def run(self):
-        self.function_reference()
-
 
 class JsonToDpg:
     def __init__(
@@ -64,7 +46,7 @@ class JsonToDpg:
         generate_keyword_file_name="",
         use_dpg_extended=True,
         debug=False,
-        async_functions=[],
+        async_functions={},
     ):
         self.dpg = dpg
         self.parse_history = []

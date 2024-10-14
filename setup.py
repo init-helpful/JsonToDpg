d51ca3f152465d40da70d8b726ea3714c9591485
init-helpful <80361624+init-helpful@users.noreply.github.com>
2024-10-14 00:29:10 -0400

    Updated Setup, Removed Plugin Object

--- setup.py
+++ setup.py
@@ -7,7 +7,14 @@ setup(
     author="init-helpful",
     author_email="init.helpful@gmail.com",
     url="https://github.com/init-helpful/JsonToDpg",
-    py_modules=["jsontodpg", "dpgkeywords", "tokenizer", "dpgextended"],
-    install_requires=["dearpygui"],
+    py_modules=[
+        "jsontodpg",
+        "dpgkeywords",
+        "tokenizer",
+        "dpgextended",
+        "controller",
+        "store",
+    ],
+    install_requires=["dearpygui", "polars"],
     package_dir={"": "src"},
 )
